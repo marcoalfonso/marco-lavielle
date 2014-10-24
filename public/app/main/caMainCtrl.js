4eb8f520b93fb7bb23008db0de3d5f67fe1e5d1b
Marco Lavielle <marcoalfonso@gmail.com>
2014-10-25 01:37:25 +1100

    Angular Data

--- public/app/main/caMainCtrl.js
+++ public/app/main/caMainCtrl.js
@@ -1,3 +1,8 @@
 angular.module('app').controller('caMainCtrl', function($scope) {
-	$scope.myVar = "Hello Angular";
+	$scope.apps = [
+	    {name: 'Commercial Real Estate', featured: true, published: new Date('10/5/2013')},
+	    {name: 'LawPath', featured: true, published: new Date('10/12/2013')},
+	    {name: 'A-lift', featured: false, published: new Date('10/1/2013')},
+	    {name: 'Ad-Roller', featured: false, published: new Date('7/12/2013')},
+	]
 });
