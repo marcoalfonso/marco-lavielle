a78548f22a6cc1b469a1820affda518d9651d99c
Marco Lavielle <marcoalfonso@gmail.com>
2014-10-24 23:52:10 +1100

    Ready to deploy

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express'),
 	stylus = require('stylus'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+//MIDDLEWARE
 function compile(str, path) {
 	return stylus(str).set('filename', path);
 }
-
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
@@ -25,14 +26,40 @@ app.use(stylus.middleware (
 ));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/partials/:partialPath', function(req, res) {
-	res.render('partials/' + req.params.partialPath);
-})
+//DATABASE
+//if (env === 'development') {
+	//mongoose.connect('mongodb://localhost/creating_apps');
+//} else {
+	//mongoose.connect('mongodb://marco:creating_apps@linus.mongohq.com:10037/creating_apps');
+//}
 
-app.get('*', function(req, res) {
-	res.render('index');
+mongoose.connect('mongodb://marco:creating_apps@linus.mongohq.com:10037/creating_apps');
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error..'));
+db.once('open', function callback() {
+	console.log('creating apps db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+	mongoMessage = messageDoc.message;
 });
 
-var port = 3030;
+//ROUTES
+app.get('/partials/:partialPath', function(req, res) {
+	res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
+});
+
+//SERVER LISTENER
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
