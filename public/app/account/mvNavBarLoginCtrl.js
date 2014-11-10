3e8fa85a20e2a680b7d6d97e3f5f5f305ea81ba9
Marco Lavielle <marcoalfonso@gmail.com>
2014-11-10 23:37:33 +1100

    Finished authentication

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,13 +1,12 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier) {
+angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth) {
 	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
-		$http.post('/login', {username:username, password:password}).then(function(response) {
-			if (response.data.success) {
-				mvIdentity.currentUser = response.data.user;
-				mvNotifier.notify('Logged In!');
+		mvAuth.authenticateUser(username, password).then(function(success) {
+			if(success) {
+				mvNotifier.notify('You have successfully signed in!');
 			} else {
 				mvNotifier.notify('Username/Password combination incorrect');
 			}
 		});
 	}
-})
+}) 
