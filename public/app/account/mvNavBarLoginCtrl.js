fa11ce2beb0c3919b848ddcbdd435791d4c60b41
Marco Lavielle <marcoalfonso@gmail.com>
2014-11-12 23:31:06 +1100

    Added bootstrap and sign out feature

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
 	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
 		mvAuth.authenticateUser(username, password).then(function(success) {
@@ -9,4 +9,13 @@ angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, m
 			}
 		});
 	}
-}) 
+
+	$scope.signout = function() {
+		mvAuth.logoutUser().then(function() {
+			$scope.username = "";
+			$scope.password = "";
+			mvNotifier.notify('You have successfully signed out!');
+			$location.path('/');
+		});
+	}
+});
--- server/config/routes.js
+++ server/config/routes.js
@@ -9,6 +9,11 @@ module.exports = function(app) {
 
 	app.post('/login', auth.authenticate);
 
+	app.post('/logout', function(req, res) {
+		req.logout();
+		res.end();
+	});
+
 	app.get('*', function(req, res) {
 		res.render('index', {
 			//mongoMessage: mongoMessage
