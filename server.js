3f076b5495626341ffea188fb04ae3a7db32c0f4
Marco Lavielle <marcoalfonso@gmail.com>
2014-12-02 10:04:39 +1100

    Added newrelic to package json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
 var newrelic = require('newrelic');
+var express = require('express');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
