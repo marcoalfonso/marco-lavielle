1499693a8f109836e0f02a54db3cd2d2efe42e20
Marco Lavielle <marcoalfonso@gmail.com>
2014-12-04 04:35:11 +1100

    About page V1

--- public/app/about/mvAboutCtrl.js
+++ public/app/about/mvAboutCtrl.js
@@ -1,3 +1,5 @@
 angular.module('app').controller('mvAboutCtrl', function($scope, $rootScope){
 	$rootScope.loaded = "loaded";
+	$rootScope.desktop = "desktop";
+	$rootScope.detected = "detected";
 })
--- public/app/main/caMainCtrl.js
+++ public/app/main/caMainCtrl.js
@@ -1,5 +1,7 @@
 angular.module('app').controller('caMainCtrl', function($scope, mvCachedClients, $rootScope) {
 	$rootScope.loaded = "loaded";
+	$rootScope.desktop = "desktop";
+	$rootScope.detected = "detected";
 	$rootScope.preview="preview-section-1";
 	$scope.clients = mvCachedClients.query();
 });
