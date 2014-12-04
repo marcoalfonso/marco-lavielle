7742847273f1d216d4b532aace72a45594cf2c29
Marco Lavielle <marcoalfonso@gmail.com>
2014-12-04 11:41:56 +1100

    About page V3

--- public/app/about/mvAboutCtrl.js
+++ public/app/about/mvAboutCtrl.js
@@ -2,4 +2,5 @@ angular.module('app').controller('mvAboutCtrl', function($scope, $rootScope){
 	$rootScope.loaded = "loaded";
 	$rootScope.desktop = "desktop";
 	$rootScope.detected = "detected";
+	$rootScope.scan = "scan-faster";
 })
