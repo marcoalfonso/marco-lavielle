2c3dbc70c76c8d5b73e5d0a09873167243bd1145
Marco Lavielle <marcoalfonso@gmail.com>
2014-12-17 16:05:26 +1100

    Added Experience page

--- public/app/clients/mvClientListCtrl.js
+++ public/app/clients/mvClientListCtrl.js
@@ -1,6 +1,7 @@
 angular.module('app').controller('mvClientListCtrl', function($scope, mvCachedClients, $rootScope){
 	$scope.clients = mvCachedClients.query();
 	$rootScope.loaded = "loaded";
+	$rootScope.desktop = "desktop";
 	$scope.sortOptions = [{value:"name", text: "Sort by Name"},
 		{value: "published", text: "Sort by Publish Date"}];
 	$scope.sortOrder = $scope.sortOptions[0].value;
--- public/app/experience/mvExperienceCtrl.js
+++ public/app/experience/mvExperienceCtrl.js
@@ -1,3 +1,4 @@
 angular.module('app').controller('mvExperienceCtrl', function($scope, $rootScope){
 	$rootScope.loaded = "loaded";
+	$rootScope.desktop = "desktop";
 })
