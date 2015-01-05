f20961f3a5e5ce283b81f2ed79d16b0d3d5b8591
Marco Lavielle <marcoalfonso@gmail.com>
2015-01-06 03:49:59 +1100

    Added spinner text

--- server/models/Client.js
+++ server/models/Client.js
@@ -4,7 +4,8 @@ var clientSchema = mongoose.Schema({
 	name: {type:String, required:'{PATH} is required!'},
 	featured: {type:Boolean, required:'{PATH} is required!'},
 	published: {type:Date, required:'{PATH} is required!'},
-	tags: [String]
+	tags: {type:String},
+	url: {type:String}
 });
 
 var Client = mongoose.model('Client', clientSchema);
@@ -12,10 +13,10 @@ var Client = mongoose.model('Client', clientSchema);
 function createDefaultClients() {
 	Client.find({}).exec(function(err, collection) {
 		if(collection.length === 0) {
-			Client.create({name: 'Commercial Real Estate', featured: true, published: new Date('10/5/2013'), tags: ['KnockoutJS']});
-		    Client.create({name: 'LawPath', featured: true, published: new Date('10/12/2013'), tags: ['Ruby On Rails']});
-		    Client.create({name: 'A-lift', featured: false, published: new Date('10/1/2013'), tags: ['Wordpress']});
-		    Client.create({name: 'Ad-Roller', featured: false, published: new Date('7/12/2013'), tags: ['Wordpress']});
+			Client.create({name: 'Commercial Real Estate', featured: true, published: new Date('10/5/2013'), tags: ['KnockoutJS'], url: "www.commercialrealestate.com.au"});
+		    Client.create({name: 'LawPath', featured: true, published: new Date('10/12/2013'), tags: ['Ruby On Rails'], url: "www.lawpath.com.au"});
+		    Client.create({name: 'A-lift', featured: false, published: new Date('10/1/2013'), tags: ['Wordpress'], url: "www.alift.co"});
+		    Client.create({name: 'Ad-Roller', featured: false, published: new Date('7/12/2013'), tags: ['Wordpress'], url: "www.ad-roller.com.au"});
 		}
 	})
 }
