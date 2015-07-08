cea348a6159bdc3981580fb3080aba94cfda3b66
Marco Lavielle <marcoalfonso@gmail.com>
2015-07-09 09:48:14 +1000

    BugFixes :D

--- public/app/clients/mvClientListCtrl.js
+++ public/app/clients/mvClientListCtrl.js
@@ -1,6 +1,7 @@
 angular.module('app').controller('mvClientListCtrl', function($scope, mvCachedClients, $rootScope){
 	$scope.clients = mvCachedClients.query();
 	$rootScope.loaded = "loaded";
+	$rootScope.levelUp = "level-0";
 	$rootScope.desktop = "desktop";
 	$rootScope.section = "preview-section-1";
 	$scope.sortOptions = [{value:"name", text: "Sort by Name"},
