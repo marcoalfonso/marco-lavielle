43f811bdbdbc56cd787483f88b84ebad8bec6a06
Marco Lavielle <marcoalfonso@gmail.com>
2015-08-04 09:52:04 +1000

    Fixed the title

--- server/models/Client.js
+++ server/models/Client.js
@@ -28,7 +28,7 @@ function createDefaultClients() {
 				url: "http://service.nsw.gov.au",
 				photo: "../images/thumbnails/service_nsw.png",
 				description: "I developed the front-end for the NSW goverment portal. I use AngularJS and Bootstrap for the UI/UX. The website has 20M+ uniques a month and has been developed to comply with WCAG standards."});
-			Client.create({name: 'CommercialRealEstate', 
+			Client.create({name: 'CRE', 
 				featured: true, 
 				published: new Date('25/10/2014'), 
 				tags: ['KnockoutJS'], 
