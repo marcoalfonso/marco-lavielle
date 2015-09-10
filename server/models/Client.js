110fff69fe433d481b3c1817da45c0882b0aafba
Marco Lavielle <marcoalfonso@gmail.com>
2015-09-10 12:12:29 +1000

    Changed date

--- server/models/Client.js
+++ server/models/Client.js
@@ -30,7 +30,7 @@ function createDefaultClients() {
 				description: "I developed the front-end for the NSW goverment portal. I use AngularJS and Bootstrap for the UI/UX. The website has 20M+ uniques a month and has been developed to comply with WCAG standards."});
 			Client.create({name: 'Sydney Morning Herald', 
 				featured: true, 
-				published: new Date('20/11/2014'), 
+				published: new Date('20/12/2014'), 
 				tags: ['Jquery'], 
 				url: "http://www.smh.com.au",
 				photo: "../images/thumbnails/sydeny_morning_herald.png",
