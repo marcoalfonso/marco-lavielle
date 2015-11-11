507ea3bc9a1aad51c6bbdbe6732b9d1007d602d1
Marco Lavielle <marcoalfonso@gmail.com>
2015-11-11 16:01:19 +1100

    Changed query

--- public/app/journal/mvJournalCtrl.js
+++ public/app/journal/mvJournalCtrl.js
@@ -2,6 +2,6 @@ angular.module('app').controller('mvJournalCtrl', function($scope, $rootScope, m
 	$rootScope.loaded = "loaded";
 	$rootScope.desktop = "desktop";
 	$rootScope.levelUp = "level-0";
-	$scope.posts = mvCachedPosts.query();
+	$scope.posts = mvPost.query();
 	$timeout = twttr.widgets.load();
 });
