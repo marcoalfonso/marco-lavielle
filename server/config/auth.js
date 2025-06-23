175f9f9b1282c699722d7f98a4346810742d4d83
Marco Lavielle <marcoalfonso@gmail.com>
2025-06-23 16:17:34 +1000

    feature - fix login issue

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,7 +4,7 @@ exports.authenticate = function(req, res, next) {
 	req.body.username = req.body.username.toLowerCase();
 	var auth = passport.authenticate('local', function(err, user) {
 		if(err) {return next(err);}
-		if(!user) { res.send({success:false})}
+		if(!user) { return res.send({success:false})}
 		req.logIn(user, function(err) {
 			if(err) {return next(err);}
 			res.send({success:true, user: user});
